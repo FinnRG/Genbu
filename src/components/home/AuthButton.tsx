31c4a38554d7c5eba02ab9eb5129252fd8ad4c47
Finn R. Gärtner <finngaertner@protonmail.com>
2022-08-17 10:07:22 +0200

    feat: Create SignUp and SignIn Dialog

--- src/components/home/AuthButton.tsx
+++ src/components/home/AuthButton.tsx
@@ -1,5 +1,36 @@
-const AuthButton: React.FC = () => {
-  return <></>
+import { Button, Modal, PasswordInput, Stack, TextInput, useMantineTheme } from '@mantine/core'
+import { useToggle } from '@mantine/hooks'
+import { useState } from 'react'
+import AuthDialog from './AuthDialog'
+
+interface AuthButtonProps {
+  style: string
+}
+
+const AuthButton: React.FC<AuthButtonProps> = (props) => {
+  const theme = useMantineTheme()
+  const [opened, setOpened] = useState<boolean>(false)
+  const [title, toggle] = useToggle(['login', 'register'])
+
+  return (
+    <>
+
+      <Modal
+        opened={opened}
+        onClose={() => setOpened(false)}
+        title={`Welcome to Mantine, ${title} with`}
+        overlayColor={theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.colors.gray[2]}
+        overlayOpacity={0.55}
+        overlayBlur={3}
+      >
+        <AuthDialog toggleTitle={toggle} title={title} />
+      </Modal>
+
+      <Button radius='xl' size='md' className={props.style} onClick={() => setOpened(true)}>
+        Log in
+      </Button>
+    </>
+  )
 }
 
 export default AuthButton
--- src/components/home/HomeView.tsx
+++ src/components/home/HomeView.tsx
@@ -7,18 +7,19 @@ import {
   Group,
   Text,
   List,
-  ThemeIcon,
-} from '@mantine/core';
-import { IconCheck } from '@tabler/icons';
-import React from 'react';
-import image from './image.svg';
+  ThemeIcon
+} from '@mantine/core'
+import { IconCheck } from '@tabler/icons'
+import React from 'react'
+import AuthButton from './AuthButton'
+import image from './image.svg'
 
 const useStyles = createStyles((theme) => ({
   inner: {
     display: 'flex',
     justifyContent: 'space-between',
     paddingTop: theme.spacing.xl * 4,
-    paddingBottom: theme.spacing.xl * 4,
+    paddingBottom: theme.spacing.xl * 4
   },
 
   content: {
@@ -27,8 +28,8 @@ const useStyles = createStyles((theme) => ({
 
     [theme.fn.smallerThan('md')]: {
       maxWidth: '100%',
-      marginRight: 0,
-    },
+      marginRight: 0
+    }
   },
 
   title: {
@@ -39,84 +40,82 @@ const useStyles = createStyles((theme) => ({
     fontWeight: 900,
 
     [theme.fn.smallerThan('xs')]: {
-      fontSize: 28,
-    },
+      fontSize: 28
+    }
   },
 
   control: {
     [theme.fn.smallerThan('xs')]: {
-      flex: 1,
-    },
+      flex: 1
+    }
   },
 
   image: {
     flex: 1,
 
     [theme.fn.smallerThan('md')]: {
-      display: 'none',
-    },
+      display: 'none'
+    }
   },
 
   highlight: {
     position: 'relative',
     backgroundColor: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).background,
     borderRadius: theme.radius.sm,
-    padding: '4px 12px',
-  },
-}));
+    padding: '4px 12px'
+  }
+}))
 
 const HomeView: React.FC = () => {
-  const { classes } = useStyles();
+  const { classes } = useStyles()
   return (
-    <div>
-      <Container>
-        <div className={classes.inner}>
-          <div className={classes.content}>
-            <Title className={classes.title}>
-              A <span className={classes.highlight}>better</span> way to <br /> take and share notes
-            </Title>
-            <Text color="dimmed" mt="md">
-              Use highly collaborative notebooks to increase your team's efficiency and productivity
-              while keeping everyone's thoughts organized.
-            </Text>
+    <Container>
+      <div className={classes.inner}>
+        <div className={classes.content}>
+          <Title className={classes.title}>
+            A <span className={classes.highlight}>better</span> way to <br /> take and share notes
+          </Title>
+          <Text color='dimmed' mt='md'>
+            Use highly collaborative notebooks to increase your team's efficiency and productivity
+            while keeping everyone's thoughts organized.
+          </Text>
 
-            <List
-              mt={30}
-              spacing="sm"
-              size="sm"
-              icon={
-                <ThemeIcon size={20} radius="xl">
-                  <IconCheck size={12} stroke={1.5} />
-                </ThemeIcon>
+          <List
+            mt={30}
+            spacing='sm'
+            size='sm'
+            icon={
+              <ThemeIcon size={20} radius='xl'>
+                <IconCheck size={12} stroke={1.5} />
+              </ThemeIcon>
               }
-            >
-              <List.Item>
-                <b>Collaborative</b> – share your ideas and thoughts with your team
-              </List.Item>
-              <List.Item>
-                <b>Privacy</b> – none of the user data will be shared in any form 
-              </List.Item>
-              <List.Item>
-                <b>Free and open source</b> – anyone can view the source code and deploy
-                their own Genbu instance
-              </List.Item>
+          >
+            <List.Item>
+              <b>Collaborative</b> – share your ideas and thoughts with your team
+            </List.Item>
+            <List.Item>
+              <b>Privacy</b> – none of the user data will be shared in any form
+            </List.Item>
+            <List.Item>
+              <b>Free and open source</b> – anyone can view the source code and deploy
+              their own Genbu instance
+            </List.Item>
 
-            </List>
+          </List>
 
-            <Group mt={30}>
-              <Button radius="xl" size="md" className={classes.control}>
-                Get started
-              </Button>
-              <Button variant="default" radius="xl" size="md" className={classes.control}>
+          <Group mt={30}>
+            <AuthButton style={classes.control} />
+            <a href='https://github.com/FinnRG/genbu'>
+              <Button variant='default' radius='xl' size='md' className={classes.control}>
                 Source code
               </Button>
-            </Group>
-          </div>
-          <Image src={image} className={classes.image} />
+            </a>
+          </Group>
         </div>
-      </Container>
-    </div>
-  );
+        <Image src={image} className={classes.image} />
+      </div>
+    </Container>
+  )
 }
 
 export default HomeView
